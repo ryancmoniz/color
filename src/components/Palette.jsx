3d344cf2c1b24abf912d45014701f898185d92af
ryancmoniz <ryancmoniz@gmail.com>
2020-07-26 23:02:16 -0400

    implement ColorBox inside Palette

--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ColorBox from './ColorBox';
 
-const Palette = () => (
-  <div className="Palette">
-    {/* TODO: NAVBAR */}
-    <div className="Palette-colors">{/* TODO: COLOR BOXES  */}</div>
-    {/* TODO: FOOTER */}
-  </div>
-);
+const Palette = ({ palette }) => {
+  const colorBoxes = palette.colors.map((color) => (
+    <ColorBox background={color.color} name={color.name} />
+  ));
+  return (
+    <div className="Palette">
+      {/* TODO: NAVBAR */}
+      <div className="Palette-colors">{colorBoxes}</div>
+      {/* TODO: FOOTER */}
+    </div>
+  );
+};
+
+Palette.propTypes = {
+  palette: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Palette;
